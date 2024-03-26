884051e6def1e84513823886d64ab34114240ef7
white-rabbit-group <ajay.devraj@whiterabbit.org>
2024-03-26 18:12:31 +0530

    test a feature

--- src/scripts/components/carousel.js
+++ src/scripts/components/carousel.js
@@ -9,6 +9,8 @@ carouselWrappers.forEach(wrapper => {
   const paginationContainer = carousel.querySelector(`.controls__pagination`);
   const prevButton = carousel.querySelector(`.controls__prev`);
   const nextButton = carousel.querySelector(`.controls__next`);
+  const firstButton = carousel.querySelector(`.controls__first`);
+  const lastButton = carousel.querySelector(`.controls__last`);
 
   const swiper = new Swiper(carousel, {
     modules: [ Pagination, Navigation ],
@@ -25,15 +27,18 @@ carouselWrappers.forEach(wrapper => {
     },
     keyboard: {
       enabled: true
-    }
+    },
+    on: {
+      init: function () {
+        firstButton.addEventListener('click', () => {
+          this.slideTo(0); // Go to the first slide
+        });
+        lastButton.addEventListener('click', () => {
+          this.slideTo(this.slides.length - 1); // Go to the last slide
+        });
+      },
+    },
   });
   
 });
 
-// Custom button click events
-$('#first-button').on('click', function() {
-  swiper.slideTo(0); // Go to the first slide
-});
-$('#last-button').on('click', function() {
-  swiper.slideTo(swiper.slides.length - 1); // Go to the last slide
-});
