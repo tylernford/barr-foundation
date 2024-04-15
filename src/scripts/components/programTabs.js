9a340b80c63cc1362f39c3362cbc3344a8d7e183
Tyler N. Ford <tylernford@gmail.com>
2024-04-15 12:14:47 -0500

    WIP Mobile tabs

--- src/scripts/components/programTabs.js
+++ src/scripts/components/programTabs.js
@@ -1,19 +1,53 @@
 const blockOpener = document.querySelector(`.blockOpener`);
-const blockTabs = document.querySelector(`.blockTabs`)
-const programAreaTriggers = document.querySelectorAll(`.blockTabs__item`);
+const programTabs = document.querySelector(`.program-tabs`)
+const programAreaTriggers = document.querySelectorAll(`.program-tabs__item`);
+const programAreaCards = document.querySelectorAll(`.program-tabs__card`);
 
-// ---------------------------------------------------------------------------
-// Reveal sub menus on trigger click
-programAreaTriggers.forEach(programAreaTrigger => {
-  programAreaTrigger.addEventListener(`mouseenter`, function(){
-    const color = programAreaTrigger.getAttribute('data-theme-color');
 
-    blockOpener.setAttribute(`data-theme-color`, color);
-    blockTabs.setAttribute(`data-theme-color`, color);
-  });
+function triggerProgramTabs(){
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
-  programAreaTrigger.addEventListener(`mouseleave`, function(){
-    blockOpener.setAttribute(`data-theme-color`, `teal`);
-    blockTabs.setAttribute(`data-theme-color`, `teal`);
-  });
-});
+  if (viewportWidth > 1280) {
+    programAreaTriggers.forEach(programAreaTrigger => {
+      programAreaTrigger.addEventListener(`mouseenter`, function(){
+        const color = programAreaTrigger.getAttribute(`data-theme-color`);
+        const target = programAreaTrigger.getAttribute(`data-target`);
+        const targetCardAttr = `[data-card="` + target + `"]`;
+        const targetCard = document.querySelector(targetCardAttr);
+    
+        blockOpener.setAttribute(`data-theme-color`, color);
+        programTabs.setAttribute(`data-theme-color`, color);
+      });
+    
+      programAreaTrigger.addEventListener(`mouseleave`, function(){
+        blockOpener.setAttribute(`data-theme-color`, `teal`);
+        programTabs.setAttribute(`data-theme-color`, `teal`);
+      });
+    });
+  } else {
+    console.log(`mobile`);
+    programAreaTriggers.forEach(programAreaTrigger => {
+      programAreaTrigger.addEventListener(`click`, function(){
+        const color = programAreaTrigger.getAttribute(`data-theme-color`);
+        const target = programAreaTrigger.getAttribute(`data-target`);
+        const targetCardAttr = `[data-card="` + target + `"]`;
+        const targetCard = document.querySelector(targetCardAttr);
+    
+        blockOpener.setAttribute(`data-theme-color`, color);
+        programTabs.setAttribute(`data-theme-color`, color);
+    
+        programAreaCards.forEach(programAreaCard => {
+          programAreaCard.classList.remove(`_is-active`);
+        });
+    
+        targetCard.classList.add(`_is-active`);
+      });
+    });
+  }
+}
+
+triggerProgramTabs();
+
+window.addEventListener(`resize`, function () {
+  triggerProgramTabs();
+}, false);
