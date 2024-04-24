6ef338bbaeee53b97568e6784f43c3ba6847fbe2
Tyler N. Ford <tylernford@gmail.com>
2024-04-24 09:50:55 -0500

    Replace main.js with code from `develop`

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,9 @@
-// Global Imports
-// import appState from './utils/appState';
-// import appForms from './utils/appForms';
-
-// Element Imports
-import './components/audio';
-import './components/accordion';
-import './components/carousel';
-import './components/footerForm';
-import './components/grants';
-import './components/navigation';
-import './components/programTabs';
-import './components/plotly';
-import './components/video';
-
-// Component Imports
-// Uncomment as needed
-// import modalDialog from './components/modalDialog';
-// import flyout from './components/flyoutContent';
-// import accordion from './components/accordions';
-// import mobileMenu from './components/mobileMenu';
-// import popupDialog from './components/popupDialog';
-
-// Inits
-// const currentState = new appState();
-
-// Component inits go here
-// Example:
-// const bioModal = new modalDialog({
-//     _id: 'modal',
-//     _hash: 'bio'
-// });
+import './components/accordion'
+import './components/audio'
+import './components/carousel'
+import './components/doughnutChart'
+import './components/fileUpload'
+import './components/footerForm'
+import './components/navigation'
+import './components/programTabs'
+import './components/video'
