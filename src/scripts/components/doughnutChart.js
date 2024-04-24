31543d4ac67ee2f7f5bf4b48c20cab90c3be2be1
Tyler N. Ford <tylernford@gmail.com>
2024-04-24 09:31:51 -0500

    Clean up doughnut charts

--- src/scripts/components/doughnutChart.js
+++ src/scripts/components/doughnutChart.js
@@ -2,33 +2,19 @@ const doughnutCharts = document.querySelectorAll(`.doughnut-chart`)
 
 
 doughnutCharts.forEach(doughnutChart => {
-  const slices        = doughnutChart.querySelectorAll(`.description__item`)
-  const sliceTriggers = doughnutChart.querySelectorAll(`.controls__button`)
+  const panels        = doughnutChart.querySelectorAll(`.description__panel`)
+  const panelTriggers = doughnutChart.querySelectorAll(`.controls__button`)
 
-  sliceTriggers.forEach(sliceTrigger => {
-    const triggerTargetHandle = sliceTrigger.getAttribute(`data-slice`)
+  panelTriggers.forEach(panelTrigger => {
+    const triggerTargetHandle = panelTrigger.getAttribute(`data-slice`)
     const triggerTarget       = doughnutChart.querySelector(`[data-slice=` + triggerTargetHandle + `]`)
     
-    sliceTrigger.addEventListener(`click`, function(){
-      slices.forEach(slice => {
-        slice.setAttribute(`data-is-active`, `false`)
+    panelTrigger.addEventListener(`click`, function(){
+      panels.forEach(panel => {
+        panel.setAttribute(`data-is-active`, `false`)
       });
 
       triggerTarget.setAttribute(`data-is-active`, `true`)
-      // blockOpener.setAttribute(`data-theme-color`, color)
-      // programTabs.setAttribute(`data-theme-color`, color)
-
-      // tabsIntro.classList.add(`_is-hidden`)
-      
-      // programAreaCards.forEach(programAreaCard => {
-      //   programAreaCard.classList.remove(`_is-active`)
-      // });
-      
-      // targetCard.classList.add(`_is-active`)
-
-      // setTimeout(() => {
-      //   tabsScrollAnchor.scrollIntoView();
-      // }, "500ms")
     })
   })
 })
